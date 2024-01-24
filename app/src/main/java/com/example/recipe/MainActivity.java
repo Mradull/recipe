371e5a03a553107b2c19bf75373aa4d5745fa542
mradull <mradul.kapoor2004@gmail.com>
2024-01-24 22:00:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recipe/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.recipe;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.recipe.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+       binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+               if(item.getItemId()==R.id.home){
+                replaceFragment(new HomeFragment());
+               }
+               else if (item.getItemId()==R.id.list)
+               {
+                   replaceFragment(new ListFragment());
+               }
+               else if (item.getItemId()==R.id.shop)
+               {
+                   replaceFragment(new ShopFragment());
+               }
+               else if (item.getItemId()==R.id.recipe)
+               {
+                   replaceFragment(new RecipeFragment());
+               }
+               else {
+                   replaceFragment(new CartFragment());
+               }
+               return true;
+           }
+       });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
