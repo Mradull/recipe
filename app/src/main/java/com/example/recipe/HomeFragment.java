bf449fb50b42dd169e2107a33bd95a4799522333
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-02-16 13:40:55 +0700

    omkar

--- app/src/main/java/com/example/recipe/HomeFragment.java
+++ app/src/main/java/com/example/recipe/HomeFragment.java
@@ -3,10 +3,15 @@ package com.example.recipe;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,7 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ViewPager viewPager;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -55,10 +61,22 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = view.findViewById(R.id.viewPager);
+        ImageSliderAdapter adapter = new ImageSliderAdapter(requireContext());
+        viewPager.setAdapter(adapter);
+
+
+
+        // Create an ArrayAdapter for the Spinner using the string array
+
+
+
+        // Set a listener for the Spinner
+
+
+        return view;
     }
 }
